1f3720ff3249c21098d5b36544ee6c3a868bdaee
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-08 14:39:04 +0000

    maillable fix

--- resources/views/mails/start.blade.php
+++ resources/views/mails/start.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
-    Hi there,
+Hi there,
 
-    Welcome aboard and thanks for joining [SMS-Verification]!
+Welcome aboard and thanks for joining [SMS-Verification]!
 
-    Please complete your payment. Your account will be setup automatically.
+Please complete your payment. Your account will be setup automatically.
 
-    @component('mail::button', ['url' => 'http://sms-verification.net/login'])
-        Login
-    @endcomponent
+@component('mail::button', ['url' => 'http://sms-verification.net/login'])
+    Login
+@endcomponent
 
-    Any Questions? Please <a href="https://sms-verification.net">Contact Us</a>.
+Any Questions? Please <a href="https://sms-verification.net">Contact Us</a>.
 @endcomponent
