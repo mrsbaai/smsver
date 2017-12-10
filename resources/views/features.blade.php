7d0f7bd8a085aa55bd55612630c88811f60a1747
mrsbaai <abdelilah.sbaai@gmail.com>
2017-12-10 11:50:34 +0000

    features updated

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -7,7 +7,6 @@
     <li><i class="fa fa-code"></i>Simple <span>API</span> Service</li>
     <li><i class="fa fa-gear"></i>Fully <span>Automated</span> System</li>
     <li><i class="fa fa-inbox"></i>Manage <span>Incoming SMS</span> In <span>Inbox</span></li>
-    <li><i class="fa fa-refresh"></i><span>Free</span> Monthly Number <span>Replacement</span></li>
 @stop
 
 @section('features2')
