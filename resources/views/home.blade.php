ae4a73b1cfe49312b02857a7e80c3a631cd69d88
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-26 17:08:47 +0100

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -134,8 +134,8 @@
     </div>
 
     <div class="col-lg-12 text-center" style="padding-top: 100px;">
-        <a href="/pricing"><img src="/img/btc.png" alt="Bitcoin Accepted Here" style="max-height: 50px; margin:30px;"></a>
-        <a href="/pricing"><img src="/img/pp.png" alt="PayPal Accepted Here" style="max-height: 50px; margin:30px;"></a>
+        <a href="/pricing"><img src="/img/btc.png" alt="Bitcoin Accepted Here" style="max-height: 30px; margin:30px;"></a>
+        <a href="/pricing"><img src="/img/pp.png" alt="PayPal Accepted Here" style="max-height: 30px; margin:30px;"></a>
     </div>
 
 
