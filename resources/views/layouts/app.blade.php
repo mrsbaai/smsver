f595949f29d19ebd8b50a4c56781e5f852128b24
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-26 19:43:03 +0100

    world

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,7 @@
     </div>
 
 
-    <div class="container col-xs-12 col-sm-12 col-lg-12 col-md-12 text-center" style="padding:0px; margin:0px;padding-top:120px;">
+    <footer class="container col-xs-12 col-sm-12 col-lg-12 col-md-12 text-center" style="padding:0px; margin:0px;padding-top:120px;">
         <div style="padding:0px;padding-top:30px;margin:0px; background-color: white;">
 		<a  href="/">SMS-Verification.net</a> &copy; 2019
         @if(!Auth::check())
@@ -95,7 +95,7 @@
         @endif
 		</div>
 
-    </div>
+    </footer>
 
 
 	
