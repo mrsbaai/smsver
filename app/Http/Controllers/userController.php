d662580d47a9e4270e6621623d360f034b99337a
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-27 02:20:08 +0100

    fix

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -137,7 +137,7 @@ class userController extends Controller
 		$discount = 100;
 		if ($request->cookie('code') !== null){
 			if ($request->cookie('code') == "15OFF"){
-				$discount = 15;
+				$discount = 85;
 				$code = $request->cookie('code');
 			}else{
 				$code = "-";
@@ -151,19 +151,19 @@ class userController extends Controller
             case 1:
                 $plan_str = "Starter";
 				$original = 300;
-                $usd = $original - ($original * $discount) / 100;
+                $usd = ($original * $discount) / 100;
                 $numbers = "200";
                 break;
             case 2:
                 $plan_str = "Business";
 				$original = 500;
-                $usd = $original - ($original * $discount) / 100;
+                $usd = ($original * $discount) / 100;
                 $numbers = "500";
                 break;
             case 3:
                 $plan_str = "Extended";
 				$original = 700;
-                $usd = $original - ($original * $discount) / 100;
+                $usd = ($original * $discount) / 100;
                 $numbers = "1000";
                 break;
         }
@@ -186,26 +186,26 @@ class userController extends Controller
 		$discount = 100;
 		if ($request->cookie('code')){
 			if ($request->cookie('code') == "15OFF"){
-				$discount = 15;
+				$discount = 85;
 			}
 			
 		}
         switch ($request->cookie('plan')) {
             case 1:
                 $plan_str = "Starter";
-                $usd = 300 - (300 * $discount) / 100;
+                $usd = (300 * $discount) / 100;
                 $btc = $this->UsdToBtc($usd);
                 $numbers = "200";
                 break;
             case 2:
                 $plan_str = "Business";
-                $usd = 500 - (500 * $discount) / 100;
+                $usd = (500 * $discount) / 100;
                 $btc = $this->UsdToBtc($usd);
                 $numbers = "500";
                 break;
             case 3:
                 $plan_str = "Extended";
-                $usd = 700 - (700 * $discount) / 100;
+                $usd = (700 * $discount) / 100;
                 $btc = $this->UsdToBtc($usd);
                 $numbers = "1000";
                 break;
@@ -225,24 +225,24 @@ class userController extends Controller
 		$discount = 100;
 		if ($request->cookie('code')){
 			if ($request->cookie('code') == "15OFF"){
-				$discount = 15;
+				$discount = 85;
 			}
 			
 		}
         switch ($request->cookie('plan')) {
             case 1:
                 $plan_str = "Starter";
-				$usd = 300 - (300 * $discount) / 100;
+				$usd = (300 * $discount) / 100;
                 $numbers = "200";
                 break;
             case 2:
                 $plan_str = "Business";
-                $usd = 500 - (500 * $discount) / 100;
+                $usd = (500 * $discount) / 100;
                 $numbers = "500";
                 break;
             case 3:
                 $plan_str = "Extended";
-                $usd = 500 - (700 * $discount) / 100;
+                $usd = (700 * $discount) / 100;
                 $numbers = "1000";
                 break;
         }
