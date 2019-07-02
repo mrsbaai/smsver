2218e291dc3f4584c06c6e1b6dff4b3f0bb7f5d7
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-02 14:40:50 +0700

    dis

--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -84,7 +84,7 @@
 			<h3 class="mb-3">Please Choose A Payment Method:</h3>
 			
 			<!-- <a href="/paypal" class="btn btn-lg paybutton paypal"><span><i class="fa fa-paypal"></i> PayPal </span></a> -->
-			<a href="/#" class="btn btn-lg paybutton btn-gray" tag="Disabled for the moment"><span><i class="fa fa-paypal"></i> PayPal </span></a>
+			<a href="/#" class="btn btn-lg paybutton btn-light" tag="Disabled for the moment"><span><i class="fa fa-paypal"></i> PayPal </span></a>
 			<a href="/bitcoin" class="btn btn-lg paybutton bitcoin"><span><i class="fa fa-bitcoin"></i> Bitcoin </span></a>
 
 		</div>
