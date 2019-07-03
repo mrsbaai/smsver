21d1ac99e2b80c0a7b83152d860f1a9ae8fe7e88
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 21:38:11 +0700

    middle

---
+++ app/Http/Middleware/CheckCountry.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class CheckCountry
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+	if ($_SERVER['HTTP_CF_IPCOUNTRY'] == "TH" or $_SERVER['HTTP_CF_IPCOUNTRY'] == "MA"){
+		return "<html><head><title>Domain Name Seized</title><META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
+	}
+
+        return $next($request);
+    }
+}
