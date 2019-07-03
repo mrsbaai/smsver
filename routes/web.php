7f5301a1c41e85aae3123387722b3d20b1c47e99
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 20:05:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Middleware\CheckPaid;
 
 
 Route::get('/seized', function () {
-return "<html><head><title>Domain Name Seized</title><META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW"></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
+return "<html><head><title>Domain Name Seized</title><META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
 });
 
 
