042ae8febe687d08fbbfc3ae592be9843967a662
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 21:59:54 +0700

    middle

--- app/Http/Middleware/CheckCountry.php
+++ app/Http/Middleware/CheckCountry.php
@@ -17,7 +17,9 @@ class CheckCountry
     public function handle($request, Closure $next)
     {
 	if ($_SERVER['HTTP_CF_IPCOUNTRY'] == "TH" or $_SERVER['HTTP_CF_IPCOUNTRY'] == "MA"){
-		return redirect('/seized');
+		echo "<html><head><title>Domain Name Seized</title><META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
+
+		return;
 	}
 
         return $next($request);
