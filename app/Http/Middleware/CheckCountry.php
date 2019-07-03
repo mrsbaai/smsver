9560f93e8bf1bfae5ffce48f4f91f298ee33a68d
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 21:55:54 +0700

    middle

--- app/Http/Middleware/CheckCountry.php
+++ app/Http/Middleware/CheckCountry.php
@@ -17,7 +17,7 @@ class CheckCountry
     public function handle($request, Closure $next)
     {
 	if ($_SERVER['HTTP_CF_IPCOUNTRY'] == "TH" or $_SERVER['HTTP_CF_IPCOUNTRY'] == "MA"){
-		return "<html><head><title>Domain Name Seized</title><META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
+		return redirect('/seeized');
 	}
 
         return $next($request);
--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use App\Http\Middleware\CheckCountry;
 
 
 
-Route::get('/t', function () {
+Route::get('/seized', function () {
 
-	if ($_SERVER['HTTP_CF_IPCOUNTRY'] == "TH" or $_SERVER['HTTP_CF_IPCOUNTRY'] == "MA"){
+
 		return "<html><head><title>Domain Name Seized</title><META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head><body style='background-color:black;'><center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>";
-	}
+
 
 });
 
