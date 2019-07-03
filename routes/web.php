1987b7d5222aa34ee27c19accd9e122d647caa1b
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 20:48:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/t', function () {
 //$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
 //echo $details->country;
 	
-	echo['HTTP_CF_IPCOUNTRY'];
+	echo $_SERVER['HTTP_CF_IPCOUNTRY'];
 });
 
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
