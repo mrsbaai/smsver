c37dcf0e53a58d3d1651ed3ff53b2b7e152af8fd
mrsbaai <abdelilah.sbaai@gmail.com>
2019-07-03 22:08:19 +0700

    middle

---
+++ resources/views/fbi.blade.php
@@ -0,0 +1,7 @@
+<html
+><head>
+<title>Domain Name Seized</title>
+<META NAME='ROBOTS' CONTENT='NOINDEX, NOFOLLOW'></head>
+<body style='background-color:black;'>
+<center><img src='https://i.imgur.com/9CpNIej.jpg'/></center>
+
