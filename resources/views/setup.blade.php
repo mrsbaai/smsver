ba52765975a2ae37557ffdd2cbfb44c78341e117
mrsbaai <abdelilah.sbaai@gmail.com>
2020-04-10 23:54:59 +0200

    gg

--- resources/views/setup.blade.php
+++ resources/views/setup.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 
-    <div class="container col-sm-12 text-center" style="padding:70px;">
+    <div class="container col-sm-12 text-center" style="padding:150px;">
 
         <h1> Your account is currently being upgraded. <br/>We will contact you as soon as it's ready.</h1>
     </div>
--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -12,7 +12,7 @@
 
 @section('content')
 
-    <div class="container col-sm-12 text-center" style="padding:70px;">
+    <div class="container col-sm-12 text-center" style="padding:150px;">
 
         <h1>Thank you for your payment!<br/><br/>We will setup your account withing <b>72 hours</b>, and contact you by email.</h1>
     </div>
