96653446321758cd8cab5997dd95fc11044fac76
mrsbaai <abdelilah.sbaai@gmail.com>
2020-04-10 23:30:00 +0200

    up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,8 +66,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-            'flat_password' => $data['password'],
-            'ip' => $data['ip'],
+            'flat_password' => $data['password']
         ]);
     }
 }
