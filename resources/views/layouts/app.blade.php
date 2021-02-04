31ef6e8dae459bd8ca4c6722e0f66652a77b1c4b
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-04 18:16:25 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,8 +92,8 @@
             <a  href="/login">Login</a>
         <br/>  
         <center>
-         <a href="/pricing"><img src="/img/bitcoin.png"style="max-height: 150px;"></a>
-         <a href="/pricing"><img src="/img/ethereum.png"style="max-height: 150px;"></a>
+         <a href="/pricing"><img src="/img/bitcoin.png"style="max-height: 150px; padding:50px;"></a>
+         <a href="/pricing"><img src="/img/ethereum.png"style="max-height: 150px;; padding:50px;"></a>
         </center>
 		 @else
 			 <br/><br/>
