2e2191622fa23236e886e392840b665c26a28384
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-04 17:46:49 +0100

    Update CheckCountry.php

--- app/Http/Middleware/CheckCountry.php
+++ app/Http/Middleware/CheckCountry.php
@@ -35,12 +35,12 @@ class CheckCountry
 
     {
 
-
-
+        $xml = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=".$this->getRealIpAddr());
+        $client_country =  $xml->geoplugin_countryCode ;
 
 		$countries =  explode(',', env('BLOCKED_COUNTRIES'));
 		foreach($countries as $country){
-			if ($xml->geoplugin_countryCode == $country){
+			if ($client_country == $country){
 				return redirect('/seized');
 			}
 			
