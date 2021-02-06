6e6ec6852fc2ac0efa87d0830eaec8ed783ba194
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-06 15:32:26 +0100

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -18,7 +18,7 @@ class userController extends Controller
 
 
     public function test(){
-		echo file_get_contents("https://receive-sms.com/ppdisposable");
+		return UsdToBtc(500) . "          --             " . UsdToEth(500);
 		
 
     }
