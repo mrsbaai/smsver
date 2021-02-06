ada27739f2180be4585575262154459b278f5581
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-06 15:21:38 +0100

    Update type.blade.php

--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -84,9 +84,8 @@
 			<h3 class="mb-3">Please Choose A Payment Method:</h3>
       
        <!-- <a href="/paypal" class="btn btn-lg paybutton paypal" aria-disabled="true"><span><i class="fa fa-paypal"></i> PayPal </span></a>  --> 
-      <a href="/ethereum" class="btn btn-lg paybutton ethereuum"><span><i class="fa fa-ethereum"></i> Ethereum </span></a>
 
-
+      <a href="/ethereum" class="btn btn-lg paybutton bitcoin"><span><i class="fa fa-bitcoin"></i> Ethereum </span></a>
       <a href="/bitcoin" class="btn btn-lg paybutton bitcoin"><span><i class="fa fa-bitcoin"></i> Bitcoin </span></a>
       
       <br/><br/>
