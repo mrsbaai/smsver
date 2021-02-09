281b8029001360ec8668c398b0bdaabc909db88d
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-09 17:05:30 +0100

    Update type.blade.php

--- resources/views/type.blade.php
+++ resources/views/type.blade.php
@@ -89,7 +89,7 @@
       <a href="/bitcoin" class="btn btn-lg paybutton bitcoin"><span><i class="fab fa-btc"></i> Bitcoin </span></a>
       
       <br/><br/>
-      <span class="label label-danger">Limited time offer: Use promo code "crypto-forever-335" to get %10 OFF</span>
+      <h4><span class="label label-danger">Limited time offer: Use promo code "crypto-forever-335" to get %10 OFF</span></h4>
 
 
 		</div>
