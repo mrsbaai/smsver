a1c894b4daa3a39d532425e8ce1b76b764de884c
mrsbaai <abdelilah.sbaai@gmail.com>
2021-02-10 15:18:31 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
                     <tbody>
                         <tr>
                             <td><b><h3 stye="padding:0px;">[+1 92 92 09 09 71]</h3></b></td>
-                            <td>United States</td>
+                            <td><h3 stye="padding:0px;">United States</h3></td>
                         </tr>
 
                     </tbody>
